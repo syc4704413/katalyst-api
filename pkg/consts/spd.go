8ddf4c966cf16f91f5a1032782bd6fe86dc9fd52
siyichao <siyichao@bytedance.com>
2026-06-12 14:38:31 +0800

    spd add mbw_usage_per_limit and cpu_usage

--- pkg/consts/spd.go
+++ pkg/consts/spd.go
@@ -51,13 +51,15 @@ const (
 
 // The fields below are multi-dim fields, mainly used to represent fine-grained resource portraits of some head services.
 const (
-	SPDAggMetricNameMultiDimCpuSimd   = "cpu_simd"
-	SPDAggMetricNameMultiDimCpuScalar = "cpu_scalar"
-	SPDAggMetricNameMultiDimCpuBranch = "cpu_branch"
-	SPDAggMetricNameMultiDimMemBw     = "mem_bw"
-	SPDAggMetricNameMultiDimMemLat    = "mem_lat"
-	SPDAggMetricNameMultiDimMemL3Pki  = "mem_l3pki"
-	SPDAggMetricNameMultiDimDiskBw    = "disk_bw"
-	SPDAggMetricNameMultiDimNetBw     = "net_bw"
-	SPDAggMetricNameMultiDimPower     = "power"
+	SPDAggMetricNameMultiDimCpuUsage         = "cpu_usage"
+	SPDAggMetricNameMultiDimCpuSimd          = "cpu_simd"
+	SPDAggMetricNameMultiDimCpuScalar        = "cpu_scalar"
+	SPDAggMetricNameMultiDimCpuBranch        = "cpu_branch"
+	SPDAggMetricNameMultiDimMemBw            = "mem_bw"
+	SPDAggMetricNameMultiDimMemLat           = "mem_lat"
+	SPDAggMetricNameMultiDimMemL3Pki         = "mem_l3pki"
+	SPDAggMetricNameMultiDimDiskBw           = "disk_bw"
+	SPDAggMetricNameMultiDimNetBw            = "net_bw"
+	SPDAggMetricNameMultiDimPower            = "power"
+	SPDAggMetricNameMultiDimMbwUsagePerLimit = "mbw_usage_per_limit"
 )
